5cba19442bf7d7267585971a930d5c3c4eeb3efb
Hoesung Jung <jrps2212@gmail.com>
2021-07-28 23:01:35 +0900

    init

---
+++ src/main/java/com/diplomacy/peaceindex/PeaceindexApplication.java
@@ -0,0 +1,13 @@
+package com.diplomacy.peaceindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceindexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomacy/peaceindex/PeaceindexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomacy.peaceindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceindexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
